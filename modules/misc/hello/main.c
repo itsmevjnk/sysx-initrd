974cf55c8fa0318987f4241c153fc3a486804fe1
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-11-02 15:01:38 +0700

    Allow kernel modules to be compiled with optimizations

--- modules/misc/hello/main.c
+++ modules/misc/hello/main.c
@@ -3,8 +3,7 @@
 void hello(); // in hello.c
 
 /* KERNEL MODULE INITIALIZATION FUNCTION */
-__attribute__((unused)) // this will be used by the kernel
-static int32_t kmod_init() {
+int32_t kmod_init() {
     hello();
     return 0;    
 }
