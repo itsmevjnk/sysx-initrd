930a8530d6cd4ec9ca6a9521504c83b25e13ffa7
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-12-03 22:51:29 +0700

    Demonstrate kernel module unloading

--- modules/misc/hello/main.c
+++ modules/misc/hello/main.c
@@ -5,5 +5,5 @@ void hello(); // in hello.c
 /* KERNEL MODULE INITIALIZATION FUNCTION */
 int32_t kmod_init() {
     hello();
-    return 0;    
+    return -1; // negative return value will result in the kernel module being unloaded upon exiting
 }
