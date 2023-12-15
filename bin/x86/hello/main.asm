c3c82149d164c1d1aac3814859fe23e9d2e5b4dc
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-12-15 11:27:08 +0700

    Updated Hello World userspace example to fork itself

--- bin/x86/hello/main.asm
+++ bin/x86/hello/main.asm
@@ -1,6 +1,11 @@
 section .data
 msg: db "Hello, userland World!", 0x0A
 len equ $ - msg
+msg_child: db "(from another, forked, process)", 0x0A
+len_child equ $ - msg_child
+
+section .bss
+child_done: dd 0
 
 section .text
 global _start
@@ -11,6 +16,26 @@ mov ecx, len
 mov edx, msg
 int 0x80
 
+mov eax, 3 ; fork
+int 0x80
+test eax, eax
+jnz .parent
+
+.child:
+mov eax, 2 ; write
+mov ebx, 1 ; stdout
+mov ecx, len_child
+mov edx, msg_child
+int 0x80
+inc dword [child_done]
+jmp .exit ; forked process also needs to exit too!
+
+.parent: ; wait for child task to finish its job
+mov eax, [child_done]
+test eax, eax
+jz .parent
+
+.exit:
 mov eax, 0 ; exit
 mov ecx, 0 ; exit code
 int 0x80
